3dfbe22accbdbd03de57047dcc9a0588a414b5fe
catekui <catherine.wanjiku@student.moringaschool.com>
2022-05-12 23:26:24 +0300

    debugging

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from flask_script import Manager,Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
+
 #Creating app instance
 app = create_app('development')
 
@@ -25,5 +26,9 @@ def test():
 def make_shell_context():
     return dict(app = app,db = db,User = User)
 
+
 if __name__ == '__main__':
     manager.run()
+
+
+
